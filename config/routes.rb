f061eb40532eac22210b84739920323733e1e2bf
Adam Jubert <adam.jubert@sema4genomics.com>
2018-12-21 16:28:07 -0500

    NU-2 Appease Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :links, only: [:show, :create]
+  resources :links, only: %i[show create]
 
   get 'top', to: 'links#top', as: 'top_links'
 end
