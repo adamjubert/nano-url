fd39e323b544de37af164c03ab319575fbe24d54
Adam Jubert <adam.jubert@sema4genomics.com>
2018-12-21 16:26:34 -0500

    NU-2 Allow JSON requests (don't protect_from_forgery)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery unless: -> { request.format.json? }
+
   def json_response(object, status = :ok)
     render json: object, status: status
   end
