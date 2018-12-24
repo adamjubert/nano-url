cbe63ab4431dd9b6879c81f6b7c35b7d25fb567e
Adam Jubert <adam.jubert@sema4genomics.com>
2018-12-23 21:58:03 -0500

    Improve puts statements

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,16 @@
 unless Link.first.present?
-  file       = File.open('./spec/data/moz_top_500.csv')
-  long_links = CSV.parse(file).flatten[0..150]
+  total_links = 149
+  file        = File.open('./spec/data/moz_top_500.csv')
+  long_links  = CSV.parse(file).flatten[0..total_links]
 
   file.close
 
-  long_links.each do |long_link|
+  long_links.each.with_index do |long_link, index|
     link         = Link.create(long_link: long_link)
-    visits_count = rand(1..5)
+    visits_count = rand(0..5)
 
     visits_count.times { link.visits.create }
 
-    puts "Created #{link.inspect} and #{visits_count} visits."
+    puts "[#{index + 1}/#{total_links + 1}] Created short link '#{link.short_link}' for '#{link.long_link}' with #{visits_count} visits."
   end
 end
